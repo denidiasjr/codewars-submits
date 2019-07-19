4abb2952b47563f685ba2f1ef797033f12b438e2
Deni Junior <deni.junior@venturus.org.br>
2019-07-19 08:55:56 -0300

    First version of nim game

--- JS/nim_the_game.js
+++ JS/nim_the_game.js
@@ -1,9 +1,33 @@
 // https://www.codewars.com/kata/nim-the-game/
 
-function chooseMove(state) {
- 
-    const nimSum = state.reduce((acum, value) => acum ^ value, 0);
-    return nimSum;
+function calculateNimSum(state) {
+  return state.reduce((acum, value) => acum ^ value, 0);
+}
 
-    // if nimSum !== 0
+function chooseMove(state) {
+  const nimSum = calculateNimSum(state);
+
+  // if nimSum !== 0
+  const computerMove = state.reduce((acum, pile, index) => {
+    if (acum) {
+      return acum;
+    }
+
+    let pileCheck = pile;
+    let stateCheck = [...state];
+    let nimSumCheck = -1;
+
+    while (pileCheck > 0) {
+      pileCheck--;
+      stateCheck[index] = pileCheck;
+
+      nimSumCheck = calculateNimSum(stateCheck);
+
+      if (nimSumCheck === 0) {
+        return [index, pile - pileCheck];
+      }
+    }
+  }, null);
+
+  return computerMove;
 }
