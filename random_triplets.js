dcf124ce9219d3fdb744336a0f131bca648144e8
Deni Junior <denidiasjr@gmail.com>
2019-07-22 18:00:55 -0300

    Start random triplets development

---
+++ random_triplets.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/53f40dff5f9d31b813000774/train/javascript
+
+const recoverSecret = function(triplets) {
+
+    // Tentativa 1: Percorrer cada vetor de trás pra frente
+    let secret = triplets.reduce((acum, triplet) => {
+    
+    }, []);
+
+    return secret.join('');
+}
+
+
+const secret1 = "whatisup"
+const triplets1 = [
+  ['t','u','p'],
+  ['w','h','i'],
+  ['t','s','u'],
+  ['a','t','s'],
+  ['h','a','p'],
+  ['t','i','s'],
+  ['w','h','s']
+]
