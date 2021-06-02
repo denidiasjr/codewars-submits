b21d3dcad8a0a03cf00a8221045f89fd26984b2e
Deni Junior <denidiasjr@gmail.com>
2021-06-02 15:25:08 -0300

    Starting solution for getPINs

---
+++ JS/getPINs.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/5263c6999e0f40dee200059d
+
+// My Solution
+function getPINs(observed) {
+  
+  const possibilitiesByNumber = [
+    ['8', '0'],
+    ['1', '2', '4'],
+    ['1', '2', '3', '5'],
+    ['2', '3', '6'],
+    ['1', '4', '5', '7'],
+    ['2', '4', '5', '6', '8'],
+    ['3', '5', '6', '9'],
+    ['4', '7', '8'],
+    ['5', '7', '8', '9', '0'],
+    ['6', '8', '9']
+  ];
+
+  const expectations = [];
+  const observedArray = observed.split('');
+
+  observedArray.forEach((enterNumber, index) => {
+    if (index === 0) {
+      return expectations.push(...possibilitiesByNumber[enterNumber]);
+    } 
+
+    expectations.forEach((expectedNumber, expectedIndex) => {
+
+    });
+  });
+}
+
+getPINs("11")
