e04ddc6df9d0069a70d01b26d0685d7deb1b3b3d
Deni Junior <denidiasjr@gmail.com>
2021-11-10 17:52:28 -0300

    Finishing rockEmSocksEmRobots

--- JS/rockEmSocksEmRobots.js
+++ JS/rockEmSocksEmRobots.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/566b490c8b164e03f8000002/
 
+// My Solution
 const defineRobotsByTurn = (robot1, robot2) => 
   robot2.speed > robot1.speed ? [robot2, robot1] : [robot1, robot2];
 
@@ -17,7 +18,7 @@ const fight = (robot1, robot2, tactics) => {
       firstRobot.currentTactic++;
     }
     
-    if (secondRobot.health < 0) {
+    if (secondRobot.health <= 0) {
       resultMessage = `${firstRobot.name} has won the fight.`;
       break;
     }
@@ -27,7 +28,7 @@ const fight = (robot1, robot2, tactics) => {
       secondRobot.currentTactic++;
     }
     
-    if (firstRobot.health < 0) {
+    if (firstRobot.health <= 0) {
       resultMessage = `${secondRobot.name} has won the fight.`;;
       break;
     }
@@ -49,6 +50,19 @@ const fight = (robot1, robot2, tactics) => {
   }
 
   return resultMessage;
+}
+
+// Best solution
+function fight(r1, r2, tactic) {
+  if (r1.speed < r2.speed) [r1, r2] = [r2, r1];  
+  while(r1.tactics.length || r2.tactics.length) {
+    if (r1.tactics.length) r2.health -= tactic[r1.tactics.shift()];
+    if (r2.health <= 0) break;
+    if (r2.tactics.length) r1.health -= tactic[r2.tactics.shift()];
+    if (r2.health <= 0) break;
+  }
+  if (r1.health == r2.health) return "The fight was a draw.";
+  return `${(r1.health>r2.health)?r1.name:r2.name} has won the fight.`;
 }
 
 const robot1 = {
