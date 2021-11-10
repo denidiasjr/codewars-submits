19afb2970f29d9c3076845aaec05fdefea9ae55a
Deni Junior <denidiasjr@gmail.com>
2021-11-10 19:07:05 -0300

    First new version of sumPairs

--- JS/sumPairs.js
+++ JS/sumPairs.js
@@ -1,22 +1,29 @@
 // My solution
-var sum_pairs=function(ints, s){
-    
-    var k = 1;
+const sumPairs = (listOfNumbers, sumValue) => {
+  let indexToJump = 1;
+  let turn = 1;
+  let chosenNumbers;
 
-    while (k < ints.length){
-        
-        for (var i = 0; i < ints.length; i++){
-            
-            if ((i+k) >= ints.length){
-                break;
-            }
-
-            if ((ints[i]+ints[i+k]) == s){
-                return [ints[i],ints[i+k]];
-            }   
-        }
-        k++;
+  while ((turn < listOfNumbers.length)) {
+    for (let i = 0; i < listOfNumbers.length - turn; i++) {
+      if (listOfNumbers[i] + listOfNumbers[i + indexToJump] === sumValue) {
+        chosenNumbers = [listOfNumbers[i], listOfNumbers[i + indexToJump]];
+        break;
+      }
     }
+
+    if (chosenNumbers) {
+      break;
+    }
+    
+    turn++;
+    indexToJump++;
+  }
+
+  return chosenNumbers;
 }
 
-console.log(sum_pairs([10,5,2,3,7,5],10));
+// console.log(sumPairs([10,5,2,3,7,5], 10));
+// console.log(sumPairs([4, 3, 2, 3, 4], 6));
+// console.log(sumPairs([0, 0, -2, 3], 2));
+console.log(sumPairs([11, 3, 7, 5], 10));
